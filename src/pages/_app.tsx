270eba56dbae45b7b94bc4dace693c99ca59652a
Fabrício Augusto <fabricio.mata@gmail.com>
2021-06-28 14:11:25 -0300

    chore: add style-components, storybook and pwa

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps /*, AppContext */ } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <meta name="description" content="Any content" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
